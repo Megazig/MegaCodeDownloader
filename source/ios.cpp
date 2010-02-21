8d91615507e4fc7615cef793465c3e77d5ad7064
megazig <megazig@hotmail.com>
2010-02-21 12:45:55 -0600

    removed unneeded files

--- source/ios.cpp
+++
@@ -1,7 +0,0 @@
-#include "ios.h"
-
-IOS::IOS( ){
-	this->titleid = 0;
-	this->latest_version = 0;
-}
-
--- source/tryme.s
+++
@@ -1,8 +0,0 @@
-.globl stuff
-stuff:
-li		%r4,	250
-li		%r5,	80
-mullw	%r4,	%r4,	%r5
-li		%r5,	2
-mullw	%r3,	%r4,	%r5
-blr
